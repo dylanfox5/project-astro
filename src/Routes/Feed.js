290d545f2d7affe9bc9837289f2990a7da789aee
Dylan Fox <dylan.fox.kc@gmail.com>
2022-07-03 22:06:29 -0500

    updating feed

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,25 +1,41 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from 'react';
+import CustomNavbar from '../Components/CustomNavbar';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="content">
-        <Container className="home">
-        <Row>
-          <Col sm={12} lg={8}>
-            <div className="animate__animated animate__fadeInLeft animate__delay-1s">
-              <h1>hello</h1>
+    const [posts, setPosts] = useState([
+        { title: 'Lorem ipsum dolor', body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam enim lectus, porttitor nec orci ornare, posuere porta leo. Praesent aliquam nibh eget porta vehicula.", author: "Luigi", id: 1 },
+        { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Yoshi", id: 2 },
+        { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Mario", id: 3 },
+        { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Koopa", id: 4 },
+        { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Peach", id: 4 },
+    ]);
+
+    return (
+        <div className="App">
+            <CustomNavbar />
+            <div className="content">
+                <Container className="feed">
+                    <Row>
+                        <Col sm={12} lg={12}>
+                            <div className="animate__animated animate__fadeIn animate__delay-1s">
+                                {posts.map((post) => (
+                                    <div className="post" key={post.id}>
+                                        <h2>{post.title}</h2>
+                                        <p>{post.author}</p>
+                                        <p>{post.body}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
-          </Col>
-          <Col sm={12} lg={4}></Col>
-        </Row>
-      </Container>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
