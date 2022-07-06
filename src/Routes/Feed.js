e28d6e64856e88b73e95325064bdfe57f5c98d8c
Dylan Fox <dylan.fox.kc@gmail.com>
2022-07-05 21:54:41 -0500

    styling post css

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,15 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import CustomNavbar from '../Components/CustomNavbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card'
 
 function App() {
     const [posts, setPosts] = useState([
-        { title: 'Lorem ipsum dolor', body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam enim lectus, porttitor nec orci ornare, posuere porta leo. Praesent aliquam nibh eget porta vehicula.", author: "Luigi", id: 1 },
+        { title: 'Lorem ipsum dolor', body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam enim lectus, porttitor nec orci ornare, posuere porta leo. Praesent aliquam nibh eget porta vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam enim lectus, porttitor nec orci ornare, posuere porta leo.", author: "Luigi", id: 1 },
         { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Yoshi", id: 2 },
-        { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Mario", id: 3 },
+        { title: 'Lorem ipsum dolor', body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam enim lectus, porttitor nec orci ornare, posuere porta leo.", author: "Mario", id: 3 },
         { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Koopa", id: 4 },
         { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Peach", id: 4 },
     ]);
@@ -21,7 +22,7 @@ function App() {
                 <Container className="feed">
                     <Row>
                         <Col sm={12} lg={12}>
-                            <div className="animate__animated animate__fadeIn animate__delay-1s">
+                            {/* <div className="animate__animated animate__fadeIn animate__delay-1s">
                                 {posts.map((post) => (
                                     <div className="post" key={post.id}>
                                         <h2>{post.title}</h2>
@@ -29,7 +30,25 @@ function App() {
                                         <p>{post.body}</p>
                                     </div>
                                 ))}
-                            </div>
+                            </div> */}
+                            {posts.map((post) => (
+                                <Card className="post animate__animated animate__fadeIn animate__delay-1s" key={post.id}>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            {post.title}
+                                        </Card.Title>
+                                        <Card.Subtitle>
+                                            {post.author}
+                                        </Card.Subtitle>
+                                        <Card.Text>
+                                            {post.body}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer className="post-footer">
+                                        <small className="text-muted">Last updated 3 mins ago</small>
+                                    </Card.Footer>
+                                </Card>
+                            ))}
                         </Col>
                     </Row>
                 </Container>
