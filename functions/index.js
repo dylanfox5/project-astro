af89d44c2c74cc30391866fff35ffc17cbb5f56e
Dylan Fox <dylan.fox.kc@gmail.com>
2022-07-17 21:26:26 -0500

    minor updates

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
+
 app.get("*", (req, res) => {
-  res.send("Hello from the API");
+  res.json({message: "Hello from server!"});
 });
+
 exports.api = functions.https.onRequest(app);
