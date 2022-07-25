1b6cc4ef830838deae7382fc95db06f138a465ae
Dylan Fox <dylan.fox.kc@gmail.com>
2022-07-24 20:39:59 -0500

    navbar functionality

--- client/src/Components/CustomNavbar.js
+++ client/src/Components/CustomNavbar.js
@@ -1,25 +1,54 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import { Outlet, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { isLoggedIn } from '../global.js';
 
 function CustomNavbar() {
+
+    const Profile = (props) => {
+        const isLoggedIn = props.isLoggedIn;
+
+        if (isLoggedIn) {
+            return <Nav.Link className="nav-item" href="#">
+                <Link to="/profile" className="link">Profile</Link>
+            </Nav.Link>
+        }
+    }
+
+    const Feed = (props) => {
+        const isLoggedIn = props.isLoggedIn;
+
+        if (isLoggedIn) {
+            return <Nav.Link className="nav-item" href="#">
+                <Link to="/feed" className="link">Feed</Link>
+            </Nav.Link>
+        }
+    }
+
+    const Account = (props) => {
+        const isLoggedIn = props.isLoggedIn;
+        if (isLoggedIn) {
+            return <Nav.Link className="nav-item">
+                <Link to="/" className="link">Account</Link>
+            </Nav.Link>
+        } else {
+            return <Nav.Link className="nav-item">
+                <Link to="/" className="link">Login</Link>
+            </Nav.Link>
+        }
+    }
+
     return (
         <Navbar sticky="top" variant="light" expand="lg" className="animate__animated animate__fadeInDown animate__delay-1s">
             <Container>
                 <Navbar.Brand href="/">Project Astro</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Nav navbarScroll>
-                        <Nav.Link className="nav-item" href="#">
-                            <Link to="/profile" className="link">Profile</Link>
-                        </Nav.Link>
-                        <Nav.Link className="nav-item" href="#">
-                            <Link to="/feed" className="link">Feed</Link>
-                        </Nav.Link>
-                        <Nav.Link className="nav-item">
-                            <Link to="/" className="link">Login</Link>
-                        </Nav.Link>   
+                        <Profile isLoggedIn={isLoggedIn} />
+                        <Feed isLoggedIn={isLoggedIn} />
+                        <Account isLoggedIn={isLoggedIn} />
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- client/src/global.js
+++ client/src/global.js
@@ -1,3 +1,5 @@
+var isLoggedIn = true;
+
 var ps = [
     { title: 'Lorem ipsum dolor', body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam enim lectus, porttitor nec orci ornare, posuere porta leo. Praesent aliquam nibh eget porta vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam enim lectus, porttitor nec orci ornare, posuere porta leo.", author: "Luigi", id: 1 },
     { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Yoshi", id: 2 },
@@ -6,4 +8,4 @@ var ps = [
     { title: 'Lorem ipsum dolor', body: "lorem ipsum...", author: "Peach", id: 5 },
 ];
 
-export { ps }
+export { ps, isLoggedIn }
