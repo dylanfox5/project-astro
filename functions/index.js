e9e091fc9779f3b3681bdb01829dfceb340857b7
Dylan Fox <dylan.fox.kc@gmail.com>
2022-07-24 20:17:46 -0500

    update function

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,12 @@ const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 
+const posts = [
+  {title: "Lorem ipsum dolor", body: "lorem ipsum...", author: "Red", id: 5},
+];
+
 app.get("*", (req, res) => {
-  res.json({message: "Hello from server!"});
+  res.json(posts);
 });
 
 exports.api = functions.https.onRequest(app);
